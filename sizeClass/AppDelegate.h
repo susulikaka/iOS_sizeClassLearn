1ed8e2988acc4b18d749147ce35e608ffef82fbd
lisuping <lisuping@lukou.com>
2016-09-09 16:26:53 +0800

    Initial Commit

---
+++ sizeClass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sizeClass
+//
+//  Created by SupingLi on 16/9/9.
+//  Copyright © 2016年 SupingLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sizeClass/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sizeClass
+//
+//  Created by SupingLi on 16/9/9.
+//  Copyright © 2016年 SupingLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
